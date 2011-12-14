86435b70e4eaa06447b8cb68eb334839c1aba298
Rami Hollingsworth <ramihollingsworth@gmail.com>
2011-12-13 21:25:30 -0700

    Phone Number check 1 down

--- Hollingsworth_Rami_project4.js
+++ Hollingsworth_Rami_project4.js
@@ -1,1 +1,17 @@
-alert("JavaScript works!");
+/*Reveiling Module Pattern
+There should be 12 functions that do something with them inside of a function 13.
+Return actual function as call
+
+get to know regex */
+
+// Check Phone number string
+
+
+function checkNumber(number) {numberRegex = /^\d\d\d-\d\d\d-\d\d\d\d$/;
+ if(!number.match(numberRegex)) {console.log("Bad");
+  return false;
+ }else{ console.log ("Good")};
+ return true;
+};
+
+checkNumber("123-456-7890")
