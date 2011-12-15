9895125f486fe117e4c655eb708fdb7271cda095
Rami Hollingsworth <ramihollingsworth@gmail.com>
2011-12-14 20:50:50 -0700

    Added different  way of putting in address

--- Hollingsworth_Rami_project4.js
+++ Hollingsworth_Rami_project4.js
@@ -5,6 +5,7 @@ Return actual function as call
 get to know regex */
 
 // Check Phone number string
+
 function checkNumber(number) {numberRegex = /^\d\d\d-\d\d\d-\d\d\d\d$/;
  if(!number.match(numberRegex)) {console.log("Bad");
   return false;
@@ -15,10 +16,10 @@ checkNumber("123-456-7890")
 
 //Check eMail string
 
-function checkEmail(email) {emailRegex = /^\a\a\a@\b\b\b.\c\c\c$/;
+function checkEmail(email) {emailRegex = ([^A-Za-z0-9_]+ @ + ([^\.A-Za-z0-9_-]) + . +([^A-Za-z0-9_]) + $);
 	if(!email.match(emailRegex)) {console.log("B");
 	return false;
 	}else{console.log ("G")};
 	return true;
 	};
-checkEmail(rami@us.com);
+checkEmail("rai@ush.com");
